1cb5affa85f2b7d364e4a6a8b2d9f23a77ad470e
nikrikpik <nikrikpik@gmail.com>
2015-06-01 16:01:22 +0200

    Vatra Komponenta
    Prekucat BP u c++ zbog nemogucnosti ili neznanja spawnovanja BP iz
    c++.....Mozda mogu i sa likom da uradim isto......Uglavnom vrlo kul
    efekat....

---
+++ Seminarski/Vatra.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Seminarski.h"
+#include "Vatra.h"
+
+
+// Sets default values
+AVatra::AVatra()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	UParticleSystemComponent * Koren = CreateDefaultSubobject<UParticleSystemComponent, UParticleSystemComponent>("FC Barselona");
+	RootComponent = Koren;
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> Vatra_Particles(TEXT("ParticleSystem'/Game/StarterContent/Particles/P_Fire.P_Fire'"));
+	//Koren->Set
+	Koren->SetTemplate(Vatra_Particles.Object);
+}
+
+// Called when the game starts or when spawned
+void AVatra::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AVatra::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
